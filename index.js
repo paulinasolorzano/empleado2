06bc023a0f8476fb4c211cece0a77c7b7159a015
paulinasolorzano <paulina.solorzano@gmail.com>
2025-02-26 12:09:51 -0600

    cambios en index

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.post('/register', (req, res) => {
 });
 
 // Inicia el servidor
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
